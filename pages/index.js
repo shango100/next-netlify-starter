ab4a35cc6dd2712df0fd97ecde5ddca9e46bdadb
shango100 <83172130+shango100@users.noreply.github.com>
2021-09-25 11:23:25 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,18 +15,27 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-    
-    <form name="dogPictures"  data-netlify="true" action="https://successin100pages.com/">
+      </main>
+        <form name="contact" method="POST" data-netlify="true">
   <p>
-    <label>Dog’s Name: <input type="text" name="name" /></label>   
+    <label>Your Name: <input type="text" name="name" /></label>   
   </p>
- 
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
   <p>
     <button type="submit">Send</button>
   </p>
 </form>
-      </main>
-
       <Footer />
     </div>
   )
