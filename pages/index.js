fa0e6089286f05c08050c03d1873bfd879f5a802
shango100 <83172130+shango100@users.noreply.github.com>
2021-09-25 17:14:19 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,26 +16,7 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-        <form name="contact" method="POST" data-netlify="true" action="/pages/success">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+        
       <Footer />
     </div>
   )
