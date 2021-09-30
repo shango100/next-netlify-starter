d3a15fa435666698b5db8fd3fa890826cd078a1d
shango100 <83172130+shango100@users.noreply.github.com>
2021-09-30 16:13:33 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,23 +16,6 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
     
-    <!-- modify this form HTML and place wherever you want your form -->
-<form
-  action="https://formspree.io/f/mwkazvbr"
-  method="POST"
-  enctype="multipart/form-data"
->
-  <label>
-    Your email:
-    <input type="email" name="_replyto">
-  </label>
-  <label>
-    Your file:
-    <input type="file" name="upload">
-  </label>
-  <button type="submit">Send</button>
-</form>
-    
       </main>
         
       <Footer />
